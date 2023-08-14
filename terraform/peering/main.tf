cd9ad0880dc06457f91f5d7cdf65cb188c89c3fb
Tomer Admon <Tomer.a@qualisystems.com>
2023-08-14 23:14:27 +0300

    .

--- terraform/peering/main.tf
+++ terraform/peering/main.tf
@@ -23,7 +23,7 @@ data "aws_vpc" "selected" {
 
 resource "aws_vpc_peering_connection" "vpc_to_vpc" {
   peer_vpc_id = var.peer_vpc_id 
-  vpc_id = data.aws_vpc.selected.id 
+  vpc_id = var.original_vpc_id 
   auto_accept = true
     tags = {
     Name = var.vpc_peering_name
