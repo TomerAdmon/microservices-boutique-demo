c66943aa56dfe9303480810d940d5b3364a51fa0
Tomer Admon <Tomer.a@qualisystems.com>
2023-08-14 22:57:47 +0300

    .

--- terraform/peering/main.tf
+++ terraform/peering/main.tf
@@ -31,7 +31,7 @@ resource "aws_vpc_peering_connection" "vpc_to_vpc" {
 }
 
 data "aws_route_tables" "rts" {
-  vpc_id = var.original_vpc_id
+  vpc_id = var.peer_vpc_id
 }
 
 resource "aws_route" "peering_routes" {
