a70bd87399a9e90af70df6dcfdfe116a2ff497d8
Tomer Admon <tomer.a@qualisystems.com>
2023-11-12 10:47:18 +0200

    Update set-namespace-permissions.sh

--- misc/scripts/set-namespace-permissions.sh
+++ misc/scripts/set-namespace-permissions.sh
@@ -7,3 +7,7 @@ NAMESPACE=$1
 
 kubectl create serviceaccount torque-dev-sa --namespace=$NAMESPACE
 kubectl create rolebinding rolebinding-$NAMESPACE --namespace=$NAMESPACE --serviceaccount=$NAMESPACE:torque-dev-sa --clusterrole=cluster-admin
+
+kubectl create serviceaccount torque-agent-sa-a03060 --namespace=$NAMESPACE
+kubectl create rolebinding rolebinding-$NAMESPACE --namespace=$NAMESPACE --serviceaccount=$NAMESPACE:torque-agent-sa-a03060 --clusterrole=cluster-admin
+
