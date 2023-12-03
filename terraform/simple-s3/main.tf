f264b8b3ad3c92a955f43f8ef0b2efebe9e1920a
DBS <96681520+davidbquali@users.noreply.github.com>
2023-12-03 17:27:46 +0200

    Create main.tf

---
+++ terraform/simple-s3/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.name
+  tags = {
+    Environment = "Test"
+  }
+}
